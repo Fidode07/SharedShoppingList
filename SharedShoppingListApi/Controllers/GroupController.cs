fed6322e56dc2324dccf9ca37a944edd29511188
Tom Schmitz <tom.schmitz16@gmail.com>
2023-08-03 03:45:34 +0200

    Added getGroups endpoint

---
+++ SharedShoppingListApi/Controllers/GroupController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedShoppingListApi.Data;
+using SharedShoppingListApi.Dtos;
+
+namespace SharedShoppingListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly MainDbContext _mainDbContext;
+
+        public GroupController(MainDbContext mainDbContext)
+        {
+            _mainDbContext = mainDbContext;
+        }
+        [HttpGet("get_groups")]
+        public async Task<ActionResult<ServiceResponse<List<GroupsDto>>>> GetGroups(GetGroupsDto getGroupsDto)
+        {
+            var serviceResponse = new ServiceResponse<List<GroupsDto>>();
+
+            if(getGroupsDto.UserId == 0)
+            {
+                serviceResponse.StatusCode = 400;
+                serviceResponse.Message = "User Id is required";
+                return await Task.FromResult(StatusCode(serviceResponse.StatusCode, serviceResponse));
+            }
+            
+            List<GroupsDto> groupsDto = new List<GroupsDto>();
+
+            var groupsFromUser = _mainDbContext.Groups.Where(group => group.Members.Any(member => member.Id == getGroupsDto.UserId));
+
+            foreach (var group in groupsFromUser)
+            {
+                groupsDto.Add(new GroupsDto
+                {
+                    Id = group.Id,
+                    Name = group.Name,
+                    Description = group.Description
+                });
+            }
+
+            serviceResponse.StatusCode = 200;
+            serviceResponse.Success = true;
+            serviceResponse.Message = "OK";
+            serviceResponse.Data = groupsDto;
+
+            return await Task.FromResult(StatusCode(serviceResponse.StatusCode, serviceResponse));
+        }
+    }
+}
---
+++ SharedShoppingListApi/Dtos/GetGroupsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedShoppingListApi.Dtos
+{
+    public class GetGroupsDto
+    {
+        public int UserId { get; set; }
+    }
+}
