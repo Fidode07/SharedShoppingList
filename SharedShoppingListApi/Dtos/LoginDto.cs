b4393d9bb0ca39ff3835185feea7b7b8989f3dfb
Fidode07 <mymainhelper@gmail.com>
2023-08-04 22:19:36 +0200

    clear branch

--- SharedShoppingListApi/Dtos/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharedShoppingListApi.Dtos
-{
-    public class LoginDto
-    {
-        public string Username { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
